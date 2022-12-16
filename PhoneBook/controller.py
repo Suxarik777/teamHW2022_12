71ff9e7855310878117853390a2b2843740fe578
Ilya Tsibul <tsigel@vikivostok.ru>
2022-12-16 22:50:05 +0300

    Исправления

--- PhoneBook/controller.py
+++ PhoneBook/controller.py
@@ -1,4 +1,4 @@
-from outputs import print_menu, menu_search
+from outputs import print_menu, menu_search, show_all, show_by_index, show_by_filter
 from menu import change_menu_position
 from edits import edit
 from inputs import input_menu_item, input_row, input_index, input_string
@@ -13,21 +13,29 @@ while menu_item != 7:
     if menu_item == 1:
         array = input_row()
     elif menu_item == 2:
-        ch_index = input_index()
+        ch_index = input_index(array
+                               )
         array = edit(ch_index, array)
     elif menu_item == 3:
-        export_file()
+        name_file = export_file()
         export_item = input_menu_item()
         if export_item == 1:
-            export_csv()
+            export_csv(array, name_file)
         if export_item == 2:
-            export_xml()
+            export_xml(array)
         if export_item == 3:
-            export_html()
+            export_html(array)
     elif menu_item == 4:
         menu_search()
         search_item = input_menu_item()
-
+        if search_item == 1:
+            show_all(array)
+        elif search_item == 2:
+            show_item = input_menu_item()
+            show_by_index(array, show_item)
+        elif search_item == 3:
+            search_item = input_string()
+            show_by_filter(array, search_item)
 
 
 #    menu_item_str = change_menu_position(menu_item)
