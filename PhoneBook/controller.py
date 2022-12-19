624e7752c7899fcc67598b805bc26510ba7e714e
Ilya Tsibul <tsigel@vikivostok.ru>
2022-12-19 20:11:38 +0300

    added import inputs.csv at the beginning and export in the end

--- PhoneBook/controller.py
+++ PhoneBook/controller.py
@@ -1,12 +1,17 @@
+import csv
 from outputs import print_menu, menu_search, show_all, show_by_index, show_by_filter
-from menu import change_menu_position
 from edits import edit
 from inputs import input_menu_item, input_row, input_index, input_string
 from exports import export_file, export_csv, export_xml, export_html
 
-
+with open('input.csv', 'r', encoding='utf-8') as csvfile:
+    file_reader = csv.reader(csvfile, delimiter=';', skipinitialspace=False)
+    array = []
+    for line, row in enumerate(file_reader):
+        if line>1:
+            file_reader_to_list = (';'.join(row)).split(';')
+            array.append(file_reader_to_list[1:])
 menu_item = 0
-array = [[]]
 while menu_item != 7:
     print_menu()
     menu_item = input_menu_item()
@@ -34,6 +39,12 @@ while menu_item != 7:
         elif search_item == 3:
             search_item = input_string()
             show_by_filter(array, search_item)
+with open(f'inputs.csv', 'w', encoding='utf-8') as file:
+    for text in array:
+        res_text = ";".join(text)
+        file.writelines(f'{res_text}; \n')
+print(f"Экспорт файла input.csv завершен.")
+
 
 
 #    menu_item_str = change_menu_position(menu_item)
