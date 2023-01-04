29e965a71d1aa8f31196917cc712c754bff33038
Suxarik777 <gultcev@mail.ru>
2023-01-04 03:08:53 +0300

    редакция 3 основных функций edit

--- PeoplesBase/edits.py
+++ PeoplesBase/edits.py
@@ -1,8 +1,8 @@
 
 
-################################
-######## обработка строк списка для user (доступ к функции только у админа)
-def delete_row_user(array, index):
+
+# функция удаления записи в базах
+def delete_row(array, index):
     print('Вы собираетесь удалить: \n', ' '.join(map(str, array[index])))
     user_in = int(input('''Вы действительно хотите удалить?: 
         0 - нет
@@ -16,21 +16,22 @@ def delete_row_user(array, index):
         return array
     else:
         print('Некорректный ввод!')
-        delete_row_user(index, array)
-    return
+        delete_row(index, array)
 
 
-def update_row_user(array, index):
-    update_row = ' '.join(map(str, array[index]))
-    print(f'Вы редактируете запись: \n{update_row}')
-    print('''введите новые данные через пробел  ''') # небходимо уточнить что именно мы вводим
+# функция редактирование записи в базах
+def update_row(array, index):
+    str_update_row = ' '.join(map(str, array[index]))
+    print(f'Вы редактируете запись: \n{str_update_row}')
+    print('введите новые данные через пробел  ')
+    # небходимо уточнить что именно мы вводим
     sub_lst = input('→ ').split()
 
-    sub_lst = forced_recording_serial_number_user(sub_lst, array[index][0])
+    sub_lst = forced_recording_id_database(sub_lst, array[index][0])
     str_sub_lst = ' '.join(map(str, sub_lst))
 
     user_in = int(input(f'''Внести изменения? 
-    Было: {update_row} 
+    Было: {str_update_row} 
     Стало: {str_sub_lst}
     0 - нет
     1 - да
@@ -44,24 +45,25 @@ def update_row_user(array, index):
         array.insert(index, sub_lst)
         return array
     elif user_in == 2:
-        return update_row_user(array, index)
+        return update_row(array, index)
     else:
         print('Некорректный ввод!, Начнем сначала :)')
-        return update_row_user(array, index)
+        return update_row(array, index)
 
 
-def add_row_user(array):
+# добавление записи в базу
+def add_row(array):
     how_to_print = ' '.join(map(str, array[0]))
-    new_number_user = what_is_serial_number_new_user(array) #ищем новый-следующий номер для пользователя по списку
-    print(f'''введите нового пользователя как в примере:  
+    new_number_user = what_is_id_new_database_entry(array)  # ищем след id для записи
+    print(f'''введите новую запись как в примере:  
     {how_to_print}
-    порядковый номер нового пользователя: {new_number_user}''')
+    id новой записи: {new_number_user}''')
     sub_lst = input('→ ').split()
 
-    sub_lst = forced_recording_serial_number_user(sub_lst, new_number_user) #принудительный чек порядкового номера
+    sub_lst = forced_recording_id_database(sub_lst, new_number_user)  # принудительный чек id
     str_sub_lst = ' '.join(map(str, sub_lst))
 
-    user_in = int(input(f'''Внести нового пользователя? 
+    user_in = int(input(f'''Внести новую запись? 
         {str_sub_lst}
         0 - нет
         1 - да
@@ -81,15 +83,18 @@ def add_row_user(array):
 
 
 
-# поиск нового порядкового номера пользователя
-def what_is_serial_number_new_user(array):
+
+########## вспомогательные функции для работы основных функций выше
+# поиск нового id номера в базе для новой записи
+def what_is_id_new_database_entry(array):
     last_index_row = len(array) - 1
     last_number_user = int(array[last_index_row][0])
     next_number_user = last_number_user + 1
     return next_number_user
 
-# Принудительная запись правильного порядкового номера пользователя
-def forced_recording_serial_number_user(sub_lst, number):
+
+# Принудительная запись правильного id номера записи
+def forced_recording_id_database(sub_lst, number):
     str_sub_lst = ' '.join(map(str, sub_lst))
     print(f'проверяем порядковый номер записи: \n{str_sub_lst} \nприсваиваем номер: {number}')
     if sub_lst[0].isdigit:
@@ -98,3 +103,4 @@ def forced_recording_serial_number_user(sub_lst, number):
     else:
         sub_lst.insert(0, number)
         return sub_lst
+
